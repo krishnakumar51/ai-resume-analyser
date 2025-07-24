97aceebb15c5115a394fdf0f976d35352fc1a75d
krishnakumar51 <godkrishnasskal@gmail.com>
2025-07-24 19:26:20 +0530

    AUthentication is done for nmow

---
+++ app/routes/home.tsx
@@ -0,0 +1,65 @@
+import { resumes } from "../../constants";
+import type { Route } from "./+types/home";
+import {NavBar} from "~/components/NavBar";
+import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "RESUMIND" },
+    { name: "description", content: "Smart Feedback for your Resume!" },
+  ];
+}
+
+export default function Home() {
+
+
+
+  const {auth} = usePuterStore();
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate("/auth?next=/");
+
+  }, [auth.isAuthenticated]);
+
+
+
+
+
+  return <main className="bg-[url('images/bg-main.svg')] bg-cover">
+
+    <NavBar/>
+
+
+
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track your Applications and Resume Ratings! </h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
+          </div>
+    
+
+    {resumes.length > 0 && (
+
+       <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+    )
+    
+    }
+    
+
+    </section>
+
+
+
+
+  </main>
+}
